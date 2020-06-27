5751ca9c1f441f85a37f8cdf45f79098e2c02cf3
w3cj <cj@null.computer>
2020-06-27 10:10:08 -0600

    :sparkles: update commands

--- public/commands/format.js
+++ public/commands/format.js
@@ -2,13 +2,21 @@ const fs = require('fs');
 
 const commands = require('./cloudbot_commands.json');
 
-const deduped = commands.reduce((all, {command, response}) => {
+const deduped = commands.cloudbot.settings.custom.reduce((all, {command, response}) => {
   all[response] = all[response] || [];
   all[response].push(command);
   return all;
 }, {});
 
-const allCommands = Object.entries(deduped).map(([response, aliases]) => ({ response, aliases }))
+const allCommands = Object.entries(deduped)
+  .map(([response, aliases]) => ({ response, aliases }))
+  .sort((a, b) => {
+    const aliasA = a.aliases.sort()[0];
+    const aliasB = b.aliases.sort()[0];
+    if (aliasA > aliasB) return 1;
+    if (aliasA < aliasB) return -1;
+    return 0;
+  });
 
 fs.writeFileSync('cloudbot_commands_dedupe.json',
   JSON.stringify(allCommands, null, 2),
