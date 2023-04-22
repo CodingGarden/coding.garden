5589394abc31104eae450bbf927689730d170a4e
w3cj <cj@null.computer>
2023-04-22 14:10:13 -0600

    :bug: revert to old calendar for now

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,6 +1,49 @@
 <script>
 	import '@fontsource/bitter/400.css';
 	import { onMount } from 'svelte';
+	let src = '';
+	onMount(() => {
+		let tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+		let cal = 'fdjrq4spg16jkpg6ahg41v3510%40group.calendar.google.com';
+		src = `https://calendar.google.com/calendar/embed?mode=week&src=${cal}&ctz=${tz}`;
+	});
+</script>
+
+<svelte:head>
+	<title>Coding Garden Calendar</title>
+</svelte:head>
+
+<div id="app">
+	<center>
+		<p>Here are the upcoming Coding Garden live streams.</p>
+		<p>Add it to your calendar: https://cdg.sh/ics</p>
+	</center>
+</div>
+<center>
+	<iframe {src} title="CodingGarden Schedule" frameborder="0" />
+</center>
+
+<style>
+	* {
+		scroll-behavior: smooth;
+	}
+	#app {
+		width: 80%;
+		margin: 1rem auto;
+	}
+	p {
+		font-family: 'Bitter', serif;
+	}
+	iframe {
+		margin: 0 auto;
+		width: 90vw;
+		height: 70vh;
+	}
+</style>
+
+<!-- <script>
+	import '@fontsource/bitter/400.css';
+	import { onMount } from 'svelte';
 	import '@fullcalendar/core/vdom';
 	import { Calendar } from '@fullcalendar/core';
 	import bootstrap5Plugin from '@fullcalendar/bootstrap5';
@@ -204,4 +247,4 @@
 			transform: rotate(360deg);
 		}
 	}
-</style>
+</style> -->
